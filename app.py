54b8ba6712e4924b849bb53808cfd5bf850766e4
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2022-04-13 21:43:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -38,30 +38,30 @@ def push_number(number):  # Push a number to the output
 
     if output.text() == '0':
         output.setText('')
-    
+
     # If last character is 0 and penultimate is an operation, delete the 0
-    if (len(output.text()) > 2 
+    if (len(output.text()) > 2
             and output.text()[-1] == '0'
             and output.text()[-2] in '+-*/'):
         delete_last_character()
-    
+
     output.setText(output.text() + str(number))
 
 
 def add_comma():  # Add a comma to the output
     output = window.output
-    
+
     # If there is a comma in any position after an operation
     # or on the first position, do nothing
     separator = ['+', '-', '*', '/']
     after_an_operation = custom_split(separator, output.text())[-1].find('.')
     if after_an_operation != -1:
         return
-    
+
     # If output is empty or the last character is an operation, add a 0
     if output.text() == '' or output.text()[-1] in '+-*/':
         output.setText(output.text() + '0')
-    
+
     output.setText(output.text() + '.')
 
 
@@ -78,11 +78,11 @@ def operation(operation):  # Add an operation to the output
     # If the last character is an operation, replace it
     if output.text()[-1] in '+-*/':
         delete_last_character()
-    
+
     # If 'Calcular automaticamente' is checked, calculate the output
     if window.actionAutoCalc.isChecked() and not output.text().isnumeric():
         calculate()
-    
+
     output.setText(output.text() + operation)
 
     # If the only character is an operation, delete it
@@ -95,11 +95,11 @@ def calculate():  # Calculate the output
 
     if output.text() == '':
         output.setText('0')
-    
+
     # If the output ends with an operation, delete it and calculate
     if output.text()[-1] in '+-*/':
         delete_last_character()
-    
+
     try:
         result = str(eval(output.text()))
     except ZeroDivisionError:
@@ -108,7 +108,7 @@ def calculate():  # Calculate the output
     except:
         pop_up('Erro', 'Um erro inesperado aconteceu')
         result = ''
-    
+
     # If result is larger than the maximum 16 digits, format it
     if len(result) > 16:
         result = f'{float(result):5.5}'
